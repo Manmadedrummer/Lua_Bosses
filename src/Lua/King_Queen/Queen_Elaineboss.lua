871f66036c1f9938a7a7b1168ad68e845c17547b
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-02-04 14:15:09 -0500

    Update Queen_Elaineboss.lua

--- src/Lua/King_Queen/Queen_Elaineboss.lua
+++ src/Lua/King_Queen/Queen_Elaineboss.lua
@@ -1,133 +1,114 @@
-function Queen_Elaine_OnCombat(Unit, Event) 
-Unit:SendChatMessage(11, 0, "Behold! Some brave champions have come... slaughter them!")
-Unit:RegisterEvent("Queen_Elaine_phaseone",1000,0)
-Unit:RegisterEvent("Queen_Elaine_reflectiveshield", 30000, 11)
-Unit:RegisterEvent("Queen_Elaine_smite", 41000, 16)
-Unit:RegisterEvent("Queen_Elaine_soulfaly", 67000, 11)
-Unit:RegisterEvent("Queen_Elaine_disruption", 78000, 10)
-Unit:RegisterEvent("Queen_Elaine_smite", 41000, 16)
-Unit:RegisterEvent("Queen_Elaine_frostnova", 43000, 13)
-Unit:RegisterEvent("Queen_Elaine_blizzard", 55000, 15)
-Unit:RegisterEvent("Queen_Elaine_cohealing", 64000, 7)
-Unit:RegisterEvent("Queen_Elaine_frostbolt", 63000, 18)
-Unit:RegisterEvent("Queen_Elaine_frostblast", 47000, 15)
-Unit:RegisterEvent("Queen_Elaine_soulfaly", 67000, 15)
-Unit:RegisterEvent("Queen_Elaine_disruption", 78000, 12)
-Unit:RegisterEvent("Queen_Elaine_flameburst", 80000, 11)
+function Queen_Elaine_OnCombat(event, creature, target)
+    creature:SendUnitSay("Behold! Some brave champions have come... slaughter them!", 0)
+    creature:RegisterEvent(Queen_Elaine_PhaseOne, 1000, 0)
+    creature:RegisterEvent(Queen_Elaine_ReflectiveShield, 30000, 11)
+    creature:RegisterEvent(Queen_Elaine_Smite, 41000, 16)
+    creature:RegisterEvent(Queen_Elaine_Soulfaly, 67000, 11)
+    creature:RegisterEvent(Queen_Elaine_Disruption, 78000, 10)
+    creature:RegisterEvent(Queen_Elaine_Smite, 41000, 16)
+    creature:RegisterEvent(Queen_Elaine_FrostNova, 43000, 13)
+    creature:RegisterEvent(Queen_Elaine_Blizzard, 55000, 15)
+    creature:RegisterEvent(Queen_Elaine_CoHealing, 64000, 7)
+    creature:RegisterEvent(Queen_Elaine_FrostBolt, 63000, 18)
+    creature:RegisterEvent(Queen_Elaine_FrostBlast, 47000, 15)
+    creature:RegisterEvent(Queen_Elaine_Soulfaly, 67000, 15)
+    creature:RegisterEvent(Queen_Elaine_Disruption, 78000, 12)
+    creature:RegisterEvent(Queen_Elaine_FlameBurst, 80000, 11)
 end
 
-function Queen_Elaine_reflectiveshield(pUnit, Event) 
-pUnit:CastSpell(66515, pUnit:GetClosestPlayer()) 
+function Queen_Elaine_ReflectiveShield(event, creature)
+    creature:CastSpell(66515, creature:GetClosestPlayer())
 end
 
-function Queen_Elaine_smite(pUnit, Event) 
-pUnit:FullCastSpellOnTarget(41471, pUnit:GetRandomPlayer(0)) 
+function Queen_Elaine_Smite(event, creature)
+    creature:FullCastSpellOnTarget(41471, creature:GetRandomPlayer(0))
 end
 
-function Queen_Elaine_cohealing(pUnit, Event) 
-pUnit:CastSpell(71133, pUnit:GetClosestPlayer()) 
+function Queen_Elaine_CoHealing(event, creature)
+    creature:CastSpell(71133, creature:GetClosestPlayer())
 end
 
-function Queen_Elaine_blizzard(pUnit, Event) 
-pUnit:FullCastSpellOnTarget(77924, pUnit:GetClosestPlayer(0)) 
+function Queen_Elaine_Blizzard(event, creature)
+    creature:FullCastSpellOnTarget(77924, creature:GetClosestPlayer(0))
 end
 
-function Queen_Elaine_frostnova(pUnit, Event) 
-pUnit:CastSpell(71929, pUnit:GetClosestPlayer()) 
+function Queen_Elaine_FrostNova(event, creature)
+    creature:CastSpell(71929, creature:GetClosestPlayer())
 end
 
-function Queen_Elaine_soulfaly(pUnit, Event) 
-pUnit:FullCastSpellOnTarget(79264, pUnit:GetRandomPlayer(0)) 
+function Queen_Elaine_Soulfaly(event, creature)
+    creature:FullCastSpellOnTarget(79264, creature:GetRandomPlayer(0))
 end
 
-function Queen_Elaine_frostblast(pUnit, Event) 
-pUnit:CastSpell(72124, pUnit:GetClosestPlayer()) 
+function Queen_Elaine_FrostBlast(event, creature)
+    creature:CastSpell(72124, creature:GetClosestPlayer())
 end
 
-function Queen_Elaine_frostbolt(pUnit, Event) 
-pUnit:FullCastSpellOnTarget(72167, pUnit:GetRandomPlayer(0)) 
+function Queen_Elaine_FrostBolt(event, creature)
+    creature:FullCastSpellOnTarget(72167, creature:GetRandomPlayer(0))
 end
 
-function Queen_Elaine_disruption(pUnit, Event) 
-pUnit:CastSpell(29310, pUnit:GetRandomPlayer(4)) 
+function Queen_Elaine_Disruption(event, creature)
+    creature:CastSpell(29310, creature:GetRandomPlayer(4))
 end
 
-function Queen_Elaine_flameburst(pUnit, Event) 
-pUnit:CastSpell(41131, pUnit:GetRandomPlayer(7)) 
+function Queen_Elaine_FlameBurst(event, creature)
+    creature:CastSpell(41131, creature:GetRandomPlayer(7))
 end
 
-function Queen_Elaine_reflectiveshield(pUnit, Event) 
-pUnit:CastSpell(66515, pUnit:GetClosestPlayer()) 
+function Queen_Elaine_PhaseOne(event, creature)
+    if creature:GetHealthPct() < 100 then
+        creature:RemoveEvents()
+        creature:FullCastSpell(41471)
+        creature:RegisterEvent(Queen_Elaine_ReflectiveShield, 30000, 11)
+        creature:RegisterEvent(Queen_Elaine_Smite, 41000, 16)
+        creature:RegisterEvent(Queen_Elaine_CoHealing, 64000, 7)
+        creature:RegisterEvent(Queen_Elaine_PhaseTwo, 1000, 0)
+    end
 end
 
-function Queen_Elaine_smite(pUnit, Event) 
-pUnit:FullCastSpellOnTarget(41471, pUnit:GetRandomPlayer(0)) 
+function Queen_Elaine_PhaseTwo(event, creature)
+    if creature:GetHealthPct() < 60 then
+        creature:RemoveEvents()
+        creature:FullCastSpell(28478, 41482)
+        creature:RegisterEvent(Queen_Elaine_ReflectiveShield, 30000, 11)
+        creature:RegisterEvent(Queen_Elaine_CoHealing, 64000, 7)
+        creature:RegisterEvent(Queen_Elaine_Blizzard, 55000, 15)
+        creature:RegisterEvent(Queen_Elaine_FrostBolt, 63000, 18)
+        creature:RegisterEvent(Queen_Elaine_FrostBlast, 47000, 15)
+        creature:RegisterEvent(Queen_Elaine_PhaseThree, 1000, 0)
+    end
 end
 
-function Queen_Elaine_cohealing(pUnit, Event) 
-pUnit:CastSpell(71133, pUnit:GetClosestPlayer()) 
+function Queen_Elaine_PhaseThree(event, creature)
+    if creature:GetHealthPct() < 25 then
+        creature:RemoveEvents()
+        creature:FullCastSpell(45442)
+        creature:RegisterEvent(Queen_Elaine_CoHealing, 64000, 7)
+        creature:RegisterEvent(Queen_Elaine_ReflectiveShield, 30000, 11)
+        creature:RegisterEvent(Queen_Elaine_FrostNova, 43000, 13)
+        creature:RegisterEvent(Queen_Elaine_Soulfaly, 67000, 11)
+        creature:RegisterEvent(Queen_Elaine_Disruption, 78000, 10)
+        creature:RegisterEvent(Queen_Elaine_FlameBurst, 80000, 11)
+    end
 end
 
-function Queen_Elaine_blizzard(pUnit, Event) 
-pUnit:FullCastSpellOnTarget(77942, pUnit:GetRandomPlayer(0)) 
+function Queen_Elaine_OnLeaveCombat(event, creature)
+    creature:RemoveEvents()
+    creature:SendUnitSay("Thats Weak...", 0)
 end
 
-function Queen_Elaine_flameburst(pUnit, Event) 
-pUnit:CastSpell(41131, pUnit:GetRandomPlayer(7)) 
+function Queen_Elaine_OnDied(event, creature)
+    creature:RemoveEvents()
+    creature:SendUnitSay("Ah!! My King, I will wait for you in the next life.", 0)
 end
 
-function Queen_Elaine_phaseone(pUnit, Event) 
-if pUnit:GetHealthPct() < 100 then 
-pUnit:RemoveEvents(); 
-pUnit:FullCastSpell(41471)
-pUnit:RegisterEvent("Queen_Elaine_reflectiveshield", 30000, 11)
-pUnit:RegisterEvent("Queen_Elaine_smite", 41000, 16)
-pUnit:RegisterEvent("Queen_Elaine_cohealing", 64000, 7)
-pUnit:RegisterEvent("Queen_Elaine_phasetwo",1000,0)
-end 
+function Queen_Elaine_OnKilledTarget(event, creature)
+    creature:SendUnitSay("Weak champions! Surrender now and maybe I will spare one life!", 0)
 end
 
-function Queen_Elaine_phasetwo(pUnit, Event) 
-if pUnit:GetHealthPct() < 60 then 
-pUnit:RemoveEvents(); 
-pUnit:FullCastSpell(28478, 41482)
-pUnit:RegisterEvent("Queen_Elaine_reflectiveshield", 30000, 11)
-pUnit:RegisterEvent("Queen_Elaine_cohealing", 64000, 7)
-pUnit:RegisterEvent("Queen_Elaine_blizzard", 55000, 15)
-pUnit:RegisterEvent("Queen_Elaine_frostbolt", 63000, 18)
-pUnit:RegisterEvent("Queen_Elaine_frostblast", 47000, 15)
-pUnit:RegisterEvent("Queen_Elaine_phasethree",1000,0)
-end 
-end
 
-function Queen_Elaine_phasethree(pUnit, Event) 
-if pUnit:GetHealthPct() < 25 then 
-pUnit:RemoveEvents(); 
-pUnit:FullCastSpell(45442)
-pUnit:RegisterEvent("Queen_Elaine_cohealing", 64000, 7)
-pUnit:RegisterEvent("Queen_Elaine_reflectiveshield", 30000, 11)
-pUnit:RegisterEvent("Queen_Elaine_frostnova", 43000, 13)
-pUnit:RegisterEvent("Queen_Elaine_soulfaly", 67000, 11)
-pUnit:RegisterEvent("Queen_Elaine_disruption", 78000, 10)
-pUnit:RegisterEvent("Queen_Elaine_flameburst", 80000, 11)
-end 
-end
-
-function Queen_Elaine_OnLeaveCombat(Unit, Event) 
-Unit:RemoveEvents() 
-Unit:SendChatMessage(11, 0, "Thats Weak...") 
-end
-
-function Queen_Elaine_OnDied(Unit, Event) 
-Unit:RemoveEvents() 
-Unit:SendChatMessage(12, 0, "Ah!! My King, I will wait for you in the next life.") 
-end
-
-function Queen_Elaine_OnKilledTarget(Unit, Event) 
-Unit:SendChatMessage(11, 0, "Weak champions! surrender now and i maby will spare one life!") 
-end
-
-RegisterUnitEvent(800801, 1, "Queen_Elaine_OnCombat")
-RegisterUnitEvent(800801, 2, "Queen_Elaine_OnLeaveCombat")
-RegisterUnitEvent(800801, 3, "Queen_Elaine_OnKilledTarget")
-RegisterUnitEvent(800801, 4, "Queen_Elaine_OnDied")
+RegisterCreatureEvent(800801, 1, Queen_Elaine_OnCombat)
+RegisterCreatureEvent(800801, 2, Queen_Elaine_OnLeaveCombat)
+RegisterCreatureEvent(800801, 3, Queen_Elaine_OnKilledTarget)
+RegisterCreatureEvent(800801, 4, Queen_Elaine_OnDied)
